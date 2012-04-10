8a19e493726c1113f2b8147566668fe434a29a9c
John Patrick <john1538@mail.ru>
2012-04-11 03:35:08 +0400

    iterMet commit. It just work on one example. Not touch anymore

--- iterationMethod/itMet.pas
+++ iterationMethod/itMet.pas
@@ -1,21 +1,27 @@
 const MAXVAL = 100;
 
-type ourtype = integer;
+type ourtype = real;
 
 type vector = array[0..MAXVAL] of ourtype;
 
 type matrix = array[0..MAXVAL] of vector;
 
 
+//------------------------------------------------------------------------TESTER FUNCTION
+
+procedure pV(a:vector); var i:integer; begin write('vector: '); for i:=1 to trunc(a[0]) do write(a[i]:0:10,'|'); writeln; end;
+
+//------------------------------------------------------------------------TESTER FUNCTION
+
 function getNorm(var a:matrix):ourtype; // IT NOT GET TRUE, BUT IT WORK THEM.( <-10000 )
-	var i,l,sum:integer;
+	var i,l:integer; sum:ourtype;
 	res:ourtype;
 	begin
 	res:=-10000; //!!!
 	for i:=1 to trunc(a[0][0]) do
-		begin 
-		sum:=1; 
-		for l:=1 to trunc(a[0][0]) do sum:=sum+a[i][l];
+		begin
+		sum:=0;
+		for l:=1 to trunc(a[0][0]) do sum:=sum+abs(a[i][l]);
 		if sum>res then res:=sum;
 		end;
 	getNorm:=res;
@@ -24,6 +30,8 @@ function getNorm(var a:matrix):ourtype; // IT NOT GET TRUE, BUT IT WORK THEM.( <
 function multiplyVectors(var a,b:vector):ourtype;
 	var i:integer; res:ourtype;
 	begin
+	//writeln('multiply'); pv(a); pv(b);
+	res:=0;
 	for i:=1 to trunc(a[0]) do
 		begin
 		res:=res+a[i]*b[i];
@@ -34,17 +42,22 @@ function multiplyVectors(var a,b:vector):ourtype;
 function addVectors(a,b:vector):vector;
 	var i:integer; res:vector;
 	begin
+	//writeln('chop',trunc(a[0]));
 	for i:=1 to trunc(a[0]) do
-		res[i]:=a[i]+b[i];
+		begin res[i]:=a[i]+b[i]; {writeln('chop');} end;
+	res[0]:=trunc(a[0]);
 	addVectors:=res;
 	end;
 	
-function subVectors(var a,b:vector):vector;
+function subVectors(a,b:vector):vector;
 	var i:integer; res:vector;
 	begin
 	for i:=1 to trunc(a[0]) do
 		res[i]:=a[i]-b[i];
+	res[0]:=trunc(a[0]);
+	//writeln(res[0]);
 	subVectors:=res;
+	//writeln('subVectors:end');
 	end;
 	
 function multiplyMatrixVector(var a:matrix; var x:vector):vector;
@@ -54,6 +67,7 @@ function multiplyMatrixVector(var a:matrix; var x:vector):vector;
 	begin
 	for i:=1 to trunc(a[0][0]) do
 		res[i]:=multiplyVectors(a[i],x);
+	res[0]:=x[0];
 	multiplyMatrixVector:=res;
 	end;
 
@@ -66,18 +80,18 @@ procedure importFromFile(var a:matrix; var b:vector; var e:real; name:string);
 	reset(f);
 	readln(f,n);
 	a[0][0]:=n;
-	writeln('ololo');
+	//writeln('ololo');
 	for i:=1 to n do
 		begin
 		a[i][0]:=n;
 		for l:=1 to n do read(f,a[i][l]);
-		writeln(a[i][l]);
+		//writeln(a[i][l]);
 		//readln(f);
 		end;
 	b[0]:=n;
 	for i:=1 to n do read(f,b[i]);
-	readln(f);
-	//readln(f,e);
+	//readln(f);
+	readln(f,e);
 	close(f);
 	end;	
 
@@ -86,13 +100,15 @@ procedure initX(var v:vector;n:integer);
 	begin v[0]:=n; for i:=1 to n do v[i]:=1; end;
 
 
-function getNextX(var a:matrix; var b:vector; x:vector):vector;
+function getNextX( a:matrix; b:vector; x:vector):vector;
 	var res:vector;
 	begin
 	res:=multiplyMatrixVector(a,x);
-	writeln('olologetNextX');
+	//pv(res);
+	//writeln('olologetNextX');
 	res:=addVectors(res,b);
-	writeln('olologetNextX');
+	//pv(res);
+	//writeln('olologetNextX');
 	getNextX:=res;
 	end;
 	
@@ -100,14 +116,24 @@ function getVectorNorm(a:vector):ourtype;
 	var i:integer; res:ourtype;
 	begin
 	res:=0;
+	//writeln(trunc(a[0]));
 	for i:=1 to trunc(a[0]) do
-		res:=res+a[i];
+		begin res:=res+a[i]; {writeln(res);} end;
 	getVectorNorm:=res;
+	//writeln('getVectorNorm:end');
 	end;
 
 function isEnd(var x1,x2:vector; e:real):boolean;
+	var k:ourtype;
 	begin
-	isEnd:=(getVectorNorm( subVectors(x1,x2) )/getVectorNorm(x2))<e;
+	//pv(x1); pv(x2);
+	//writeln('1:',getVectorNorm( subVectors(x1,x2) ));
+	//writeln('2:',getVectorNorm( x2 ));
+	//writeln(e:0:5);
+	//isEnd:=false;
+	k:=(getVectorNorm( subVectors(x1,x2) )/getVectorNorm(x2));
+	writeln('/~e:',k:0:5);
+	isEnd:=abs(k)<e;
 	end;
 	
 var
@@ -120,20 +146,29 @@ e:real;
 begin
 
 importFromFile(a,b,e,'input.txt');
-writeln('ololo');
+//writeln('Norm: ',getNorm(a):0:4);
+if( getNorm(a) >= 1 ) then
+	begin writeln('Norm is bad: ' , getNorm(a):0:4); exit; end;
+//readln;
+//exit;
+//writeln('BEGIN');
 initX(x1,trunc(b[0]));
+
+//pv(b); pv(x1);
 
 x2:=getNextX(a,b,x1);
 
 i:=10000;
-writeln('ololo!!!');
+//writeln('BEGIN:end');
 while ( not(isEnd(x1,x2,e)) and (i>0) ) do
 	begin
+	//writeln('[+++++++++]i= ',i);
 	i:=i-1;
 	x1:=x2;
 	x2:=getNextX(a,b,x2);
+	pv(x2);
 	end;
 write('result: ');
-for i:=1 to trunc(x2[0]) do write(x2[i],' ');
+pv(x2);
 
 end.
