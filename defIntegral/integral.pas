bef2e6ca698c2c3f45507c1951f6e6e6f577be13
John Patrick <john1538@mail.ru>
2012-04-25 14:42:31 +0400

    Integral 0.1 complet. Now I am going to imformatic.

--- defIntegral/integral.pas
+++ defIntegral/integral.pas
@@ -1,40 +1,75 @@
-//type func = function(a:real):real; //NOT WORK
-
-type info = record
-	sum:real;
-	y:real;
-	end;
-
-//type heap = array[0..1000] of info;
+uses graph,crt,sysutils;
 
 const POLENULLX = 30;
-const POLENULLY = 300;
-const POLEENDX = 330;
-const POLEENDY = 0;
+const POLENULLY = 830;
+const POLEENDX = 830;
+const POLEENDY = 30;
 
 const DEL = 2;
 
 var
 i:integer;
-from,too,y1,y2:real;
+from,too,y1,y2,e,k:real;
 n:integer;
 
+lsum,sum:real;
 
-//test:func;
+gd,gm:integer;
 
-procedure importFromFile(var from,too,y1,y2:real; var n:integer; name:string);
+x0,y0:real;
+
+xb,yb,xe,ye:real;
+
+ch:char;
+
+
+
+//------------------------------------------------ GRAPHIC FUNCTION
+
+procedure plotLine(x1,y1,x2,y2: real);
+	begin line(POLENULLX+trunc(x1),POLENULLY-trunc(y1),POLENULLX+trunc(x2),POLENULLY-trunc(y2)); end;
+	
+procedure convertCord(x,y:real; var xx,yy:real);
+	begin
+	xx:=x0+x/(abs(from)+abs(too))*(POLEENDX-POLENULLX);
+	yy:=y0+y/(abs(y1)+abs(y2))*(POLENULLY-POLEENDY);
+	end;
+
+procedure plotPole(from,too,y1,y2:real);
+	begin
+	from:=abs(from); writeln(from);
+	too:=abs(too);
+	y1:=abs(y1);
+	y2:=abs(y2);
+	plotLine( (from/(from+too))*(POLEENDX-POLENULLX),POLEENDY,(from/(from+too))*(POLEENDX-POLENULLX),POLENULLY);
+	plotLine( 0,(y1/(y1+y2))*(POLENULLY-POLEENDY),POLEENDX,(y1/(y1+y2))*(POLENULLY-POLEENDY));
+	x0:=(from/(from+too))*(POLEENDX-POLENULLX);
+	y0:=(y1/(y1+y2))*(POLENULLY-POLEENDY);
+	end;
+	
+procedure echoE(k:real;n:integer);
+	begin
+	OutTextXY(POLENULLX,POLENULLY+10*n,FloatToStr(k));
+	end;
+
+//------------------------------------------------- GRAPHIC FUNCTION
+
+
+procedure importFromFile(var from,too,y1,y2,e:real; var n:integer; name:string);
 	var f:text;
-	i,n,l:integer;
+	i,l:integer;
 	begin
 	assign(f,name);
 	reset(f);
+	writeln('ololo');
 	readln(f,from);
-//	readln(f,too); // JOHN1540M CHANGE THIS STRING
+	readln(f,too);
 	readln(f,y1);
-
-//DELL END OF FUNCTION. 
-
-
+	readln(f,y2);
+	readln(f,e);
+	readln(f,n);
+	close(f);
+	end;
 
 
 
@@ -42,62 +77,69 @@ procedure importFromFile(var from,too,y1,y2:real; var n:integer; name:string);
 //-------------------------------------------------- WORK FUNCTION
 function work_func(a:real):real;
 	begin
-	work_func:=a*sin(a);
+	//work_func:=a*sin(a);
+	work_func:=sin(a);
 	end;
-//--------------------------------------------------
-
-
-{$F+}
-{function test(a:real):real; // NOT WORK
-	begin
-	test:=a;
-	end;}
-{$F-}
-
-
 //------------------------------------------------
 
 function getIntegralSum(lastSum:real; from,too:real; n:integer):real;
-	var i:integer; h,res:real;
+	var i:integer; x,y,lx,ly,h,res,xx,yy:real;
 	begin
 	h:=(too-from)/n;
 	res:=lastSum/DEL;
+	lx:=xb; ly:=yb;
 	i:=1;
 	while (i<=n-1) do//for i:=1 to n-1 do
 		begin
-		res:=res+work_func(from+h*i)*h;
+		x:=from+h*i;
+		y:=work_func(x);
+		res:=res+y*h;
+		convertCord(x,y,x,y);
+		//-convertCord(lx,ly,lx,ly); not need
+		//writeln(x:0:2,' ',y:0:2,' ',lx:0:2,' ',ly:0:2);
+		plotLine(x,y,lx,ly);
+		lx:=x; ly:=y;
 		i:=i+2;
 		end;
+	//convertCord(x,y,x,y);
+	plotLine(x,y,xe,ye);
 	getIntegralSum:=res;
 	end;
 
-//------------------------------------------------
 
-function getFunctionValue(f:func;t:real):real;// NOT WORK
-	begin
-	getFunctionValue:=f(t);
-	end;
-
-	
-procedure plotLine(x,y: real);
-	begin end;
-
-
-	
-//--------------------------------------------------
 
 begin
 
-importFromFile();
+gd:=Detect;
+InitGraph(gd,gm,'');
+writeln('lsum');
+importFromFile(from,too,y1,y2,e,n,'input.txt');
+plotPole(from,too,y1,y2);
 
-//==========================================================
-//THIS IS WHAT I WANT TO CHANGE IN MASTER BRANCH (THINK SO)
-//==========================================================
-//BRANCH MASTER
-//ÒÅÑÒ ÍÀ ÏÅÐÅÄÀ×Ó ÐÓÑÑÊÎÃÎ ßÇÛÊÀ ×ÅÐÅÇ ÐÅÏÎÇÈÒÎÐÈÉ
-//åùå òåñò íà ïåðåäà÷ó ðóññêîãî ÿçûêà
-//=========================================================
-//THIS IS WHAT I WANT TO CHANGE IN MASTER BRANCH (THINK SO)
-//==========================================================
+lsum:=(too-from)*( work_func(from)+work_func(too) )/2;
+writeln('lsum');
+convertCord(from,work_func(from),xb,yb);
+convertCord(too,work_func(too),xe,ye);
+writeln(xb:0:2,' ',yb:0:2,' ',xe:0:2,' ',ye:0:2);
+writeln(x0:0:2,' ',y0:0:2);
+
+sum:=getIntegralSum(lsum,from,too,n);
+
+k:=abs(lsum-sum);
+writeln('begin Cicle');
+while( k>n ) do
+	begin
+	//readln(e);
+	//ch:=ReadKey;
+	//if(ch='x') then break;
+	echoE(sum,n);
+	writeln(k);
+	n:=n*2;
+	lsum:=sum;
+	sum:=getIntegralSum(lsum,from,too,n);
+	k:=abs(lsum-sum);
+	end;
+
+readln;
 
 end.
